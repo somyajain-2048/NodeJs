dd504870b11552f81127103b36c7112d9a5b40dd
somyajain-2048 <142176506+somyajain-2048@users.noreply.github.com>
2025-08-19 23:11:23 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,22 @@
+# class Person:
+#   name = "harry"
+#   occupation = "software developer"
+#   networth : 10000
+
+#   def info(self):
+#     print(f"{self.name} is a {self.occupation}")
+
+#     a = Person()
+    
+    # a.name = "shubham"
+    # a.occupation = "HR"
+
+    # print(a.name,a.occupation)
+    # a.info()
+
+# f = open('functions.py','r')
+# # print(f)
+# text = f.read()
+# print(text)
+# f.close()
+
